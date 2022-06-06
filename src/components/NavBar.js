ee94ab3d77078ce0850e7ae71cae34d71d2f635e
willyc <willcowendrums@gmail.com>
2022-06-06 15:40:18 +0100

    added logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
-import { Link } from 'gatsby'
-import React from 'react'
-
+import { Link } from "gatsby"
+import React from "react"
 
 export default function NavBar() {
   return (
     <nav>
-      <h1>William Cowen</h1>
+      <div className="logo-name">
+        <img src="./mylogo.jpg" alt="logo" height="50"></img>
+        <h1>William Cowen</h1>
+      </div>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
