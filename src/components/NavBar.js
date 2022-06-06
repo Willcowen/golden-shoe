fd80b0d8b23362565230e73066910159e9cbb491
willyc <willcowendrums@gmail.com>
2022-06-06 19:24:27 +0100

    added picture

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,6 @@ import React from "react"
 export default function NavBar() {
   return (
     <nav>
-      <div className="logo-name">
-        <img src="./mylogo.jpg" alt="logo" height="50"></img>
-        <h1>William Cowen</h1>
-      </div>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
