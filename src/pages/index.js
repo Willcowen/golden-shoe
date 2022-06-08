3bea300c5c1dbacf146a5b757f9b9eab2a4f3d7c
willyc <willcowendrums@gmail.com>
2022-06-08 15:06:33 +0100

    removed quote generator

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,6 @@ import { useEffect, useState } from 'react'
 
 export default function Home() {
 
-  const [displayQuote, setDisplayQuote] = useState('')
-
-  useEffect(() => {
-    fetch('https://zenquotes.io/api/random')
-    .then(res => {
-      console.log(res)
-    })
-  })
   return (
     <Layout>
       <section className={header}>
@@ -27,7 +19,13 @@ export default function Home() {
       </section>
       <div className="intro">
         <img src='./coder-sketch.png' />
-        <p>{displayQuote}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </Layout>
   )
